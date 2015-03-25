cab197fac5d224fe5f0dcaec984c6ebe7964bf9d
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-03-25 09:09:20 +0200

    Moved url->xexpr + find* out to gonz/html-tree

--- vimcast-fetch.rkt
+++ vimcast-fetch.rkt
@@ -3,36 +3,8 @@
 (require racket/match
 	 racket/list
 	 racket/system
-	 racket/port
-	 net/url
-	 xml
-	 xml/path
-	 html
-	 gonz/human-time)
-
-(permissive-xexprs #t)
-
-(define (url->xexprs base-url)
-  (map xml->xexpr
-       (call/input-url (string->url base-url)
-		       get-pure-port
-		       read-html-as-xml)))
-
-(define (find* root element-type)
-  (match root
-    ['() '()]
-    [(list (list sub-exprs ...) exprs ...)
-     (append (find* sub-exprs element-type)
-	     (find* exprs element-type))]
-    [(list expr exprs ...)
-     #:when (equal? element-type expr)
-     (append `((,expr ,exprs)) (find* exprs element-type))]
-    [(list expr exprs ...)
-     (find* exprs element-type)]))
-
-(define (find-link xexprs filetype)
-  (filetype-link (find* xexprs 'a)
-		      filetype))
+	 gonz/human-time
+	 gonz/html-tree)
 
 (define (link-data link)
   (match link
@@ -46,13 +18,17 @@
 						(cdr (link-data link))))
 			       links)))))
 
+(define (find-link xexprs filetype)
+  (filetype-link (find* xexprs 'a)
+		 filetype))
+
 (define (download-cast episode-number filetype)
   (define dir-url (format "~a~a/"
-			       "http://media.vimcasts.org/videos/"
-			       episode-number))
+			  "http://media.vimcasts.org/videos/"
+			  episode-number))
   (define filename (filetype-link (find* (url->xexprs dir-url)
-							   'a)
-				       filetype))
+					 'a)
+				  filetype))
   (define download-url (string-append dir-url filename))
 
   (system (format "http --download ~a --output ~a_~a"
