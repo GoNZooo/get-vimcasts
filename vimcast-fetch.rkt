fc508071940ad3860620f4f0a0bbf949d9c887af
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-06-20 12:57:50 +0300

    Fixed formatting

--- vimcast-fetch.rkt
+++ vimcast-fetch.rkt
@@ -1,10 +1,10 @@
 #lang racket/base
 
 (require racket/match
-	 racket/list
-	 racket/system
-	 gonz/human-time
-	 gonz/html-tree)
+         racket/list
+         racket/system
+         gonz/human-time
+         gonz/html-tree)
 
 (define (link-data link)
   (match link
@@ -14,30 +14,30 @@
 
 (define (filetype-link links filetype)
   (cdr (link-data (car (filter (lambda (link)
-				 (regexp-match? (pregexp filetype)
-						(cdr (link-data link))))
-			       links)))))
+                                 (regexp-match? (pregexp filetype)
+                                                (cdr (link-data link))))
+                               links)))))
 
 (define (find-link xexprs filetype)
   (filetype-link (find* xexprs 'a)
-		 filetype))
+                 filetype))
 
 (define (download-cast episode-number filetype)
   (define dir-url (format "~a~a/"
-			  "http://media.vimcasts.org/videos/"
-			  episode-number))
+                          "http://media.vimcasts.org/videos/"
+                          episode-number))
   (define filename (filetype-link (find* (url->xexprs dir-url)
-					 'a)
-				  filetype))
+                                         'a)
+                                  filetype))
   (define download-url (string-append dir-url filename))
 
   (system (format "http --download ~a --output ~a_~a"
-		  download-url episode-number filename)
-	  #:set-pwd? #t))
+                  download-url episode-number filename)
+          #:set-pwd? #t))
 
 (module+ main
   (define episode-numbers (range 1 68))
   (for-each (lambda (episode-number)
-	      (download-cast episode-number "m4v")
-	      (sleep (human-time 1m)))
-	    episode-numbers))
+              (download-cast episode-number "m4v")
+              (sleep (human-time 1m)))
+            episode-numbers))
